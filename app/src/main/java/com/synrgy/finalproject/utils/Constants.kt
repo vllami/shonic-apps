1f8c4b62d38089c8a2af31642dbc02126b3139d1
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-05-30 13:47:52 +0700

    Added SPLASH_DELAY for Splash Screen duration

--- app/src/main/java/com/synrgy/finalproject/utils/Constants.kt
+++ app/src/main/java/com/synrgy/finalproject/utils/Constants.kt
@@ -1,6 +1,8 @@
 package com.synrgy.finalproject.utils
 
 object Constants {
+    const val SPLASH_DELAY = 3_000
+
     const val BASE_URL_TESTING = ""
     const val BASE_URL_PRODUCTION = ""
     const val BASE_URL_MOCK = ""
