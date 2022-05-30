dd67018d76f68059e79b999493c9995a586cec42
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-05-30 13:49:29 +0700

    Optimize Imports for SPLASH_DELAY in Constants

--- app/src/main/java/com/synrgy/finalproject/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/synrgy/finalproject/ui/SplashScreenActivity.kt
@@ -2,12 +2,13 @@ package com.synrgy.finalproject.ui
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import com.synrgy.finalproject.ui.auth.login.LogInActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.synrgy.finalproject.databinding.ActivitySplashScreenBinding
+import com.synrgy.finalproject.ui.auth.login.LogInActivity
+import com.synrgy.finalproject.utils.Constants.SPLASH_DELAY
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
@@ -27,10 +28,6 @@ class SplashScreenActivity : AppCompatActivity() {
                 finish()
             }
         }, SPLASH_DELAY.toLong())
-    }
-
-    companion object {
-        const val SPLASH_DELAY = 3_000
     }
 
 }
