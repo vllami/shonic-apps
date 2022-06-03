0e0b6a4cc38112b4e5a7a6519cc993c7c5a9c0d0
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-06-03 19:50:28 +0700

    Added PasswordChangedActivity

---
+++ app/src/main/java/com/synrgy/finalproject/ui/auth/login/password/passwordchanged/PasswordChangedActivity.kt
@@ -0,0 +1,14 @@
+package com.synrgy.finalproject.ui.auth.login.password.passwordchanged
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.synrgy.finalproject.R
+
+class PasswordChangedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_changed)
+    }
+
+}
