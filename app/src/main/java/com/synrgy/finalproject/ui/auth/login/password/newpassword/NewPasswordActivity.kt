0f5392db55c7e7698ffefbbccdb10084655df756
Villa Mukti Indriyanto <villamuktiindriyanto@gmail.com>
2022-06-03 19:08:14 +0700

    Added NewPasswordActivity

---
+++ app/src/main/java/com/synrgy/finalproject/ui/auth/login/password/newpassword/NewPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.synrgy.finalproject.ui.auth.login.password.newpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.synrgy.finalproject.R
+
+class NewPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_password)
+    }
+}
